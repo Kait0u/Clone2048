6910821f7e7c197ce3f5ccccd72ddc8d1b9da378
kait0u <jjaw.stuff@gmail.com>
2024-09-06 11:10:58 +0200

    Javadocs added

--- core/src/main/java/pl/kaitou_dev/clone2048/Constants.java
+++ core/src/main/java/pl/kaitou_dev/clone2048/Constants.java
@@ -1,10 +1,27 @@
 package pl.kaitou_dev.clone2048;
 
 public class Constants {
+    /**
+     * The width of the resolution at which the game should be rendered.
+     */
     public static final int GAME_WIDTH = 800;
+    /**
+     * The height of the resolution at which the game should be rendered.
+     */
     public static final int GAME_HEIGHT = 600;
+
+    /**
+     * The max width of the window that displays the game.
+     */
     public static final int MAX_WIDTH = 7680;
+    /**
+     * The max height of the window that displays the game.
+     */
     public static final int MAX_HEIGHT = 4320;
+
+    /**
+     * The default delta value in case it is not specified.
+     */
     public static final float DEFAULT_DELTA = 1f / 60f;
     public static final int SLOT_SIZE = 100;
     public static final float BASIC_MOVEMENT_SPEED = 0.075f;
