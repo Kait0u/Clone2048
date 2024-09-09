9b1e4e6e7c1407e6125edbe62a4d180f7bb63bd4
kait0u <jjaw.stuff@gmail.com>
2024-09-09 14:47:34 +0200

    Javadocs improved.

---
+++ core/src/main/java/pl/kaitou_dev/clone2048/utils/timed_actions/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Contains classes representing actions that take time to finish
+ * and whose results are to be seen on the go. Their progress is measured using the delta-time.
+ */
+package pl.kaitou_dev.clone2048.utils.timed_actions;
